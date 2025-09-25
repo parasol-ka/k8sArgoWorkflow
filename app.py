489a987b1a2e4607010016435a9c91684eb04f31
parasol-ka <ananastya.chau@gmail.com>
2025-09-25 14:06:33 +0200

    test redeployement

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h3 style='color: pink; display:flex; justify-content: center; align-items: center; height: 90vh; font-size: 4em;'><b>BONJORNOOOOOOOOOOOOOOO</b></h3>"
+    return "<h3 style='color: pink; display:flex; justify-content: center; align-items: center; height: 80vh; font-size: 4em;'><b>BONJORNOOOOOOOOOOOOOOO</b></h3>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
